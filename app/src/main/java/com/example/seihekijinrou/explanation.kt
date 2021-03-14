cd874d0e4d8e6c2a86e812e153e7e84c94f14aec
syota-g <syosatci@gmail.com>
2021-03-15 04:04:54 +0900

    ルール説明画面にスライドショーを設置しました。

--- app/src/main/java/com/example/seihekijinrou/explanation.kt
+++ app/src/main/java/com/example/seihekijinrou/explanation.kt
@@ -6,18 +6,22 @@ import android.os.Bundle
 import com.example.seihekijinrou.databinding.ActivityExplanationBinding
 
 class explanation : AppCompatActivity() {
-    private lateinit var binding :ActivityExplanationBinding
+    private lateinit var binding: ActivityExplanationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityExplanationBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.start.setOnClickListener{
-            var intent = Intent(this,numberofpeople::class.java)
+        binding.start.setOnClickListener {
+            var intent = Intent(this, numberofpeople::class.java)
             startActivity(intent)
 
         }
+        binding.checkrules.setOnClickListener {
+            var intent = Intent(this, checkrules::class.java)
+            startActivity(intent)
 
 
+        }
     }
 }
